a4727982d24fc35fecc07190807cca863e1a8c54
Michael Mattingly <mikeamatt1@hotmail.com>
2023-07-07 11:24:10 -0400

    Added loadTasks function.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,5 +3,15 @@ dayjs.extend(dayjs_plugin_advancedFormat);
 //This wrapper function ensures that the DOM is fully loaded before executing the code.
 $(function () {
 
+  //Load any saved tasks from local storage if available.
+  function loadTasks() {
+    $(".time-block").each(function () {
+      const id = $(this).attr("id");
+      const task = localStorage.getItem(id);
+      if (task !== null) {
+        $(this).children(".description").val(task);
+      }
+    });
+  }
   
 });
